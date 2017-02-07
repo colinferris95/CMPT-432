8d9c658736f055af5a3a1f3cd5af38af5c995840
Colin <colinferris95@gmail.com>
2017-02-06 21:04:04 -0500

    inital planning along with some css/jquery dependencies
    includes bootstrap and lined text area along with some code planning

--- JS/lexer.js
+++ JS/lexer.js
@@ -1,1 +1,32 @@
 //This script is under construction
+/*
+
+//structure of a lex program
+
+//declarations
+/*
+
+//regular expressions and defintions
+// pattern {action}
+
+
+*/
+
+//translation rules
+/*
+
+
+
+
+
+*/
+
+//auxiliary functions
+
+/*
+
+
+
+
+
+*/
