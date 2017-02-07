6b07624254dd0688fd2ee82375209c54694ae6e5
Colin <colinferris95@gmail.com>
2017-02-06 22:32:47 -0500

    proof of concept scanning for char
    very basic concept of scanning each character of text and identifying
    its a letter

--- JS/lexer.js
+++ JS/lexer.js
@@ -1,16 +1,28 @@
 //This script is under construction
-/*
+
 
 //structure of a lex program
 
+
+
+ 
 //declarations
-/*
+
 
 //regular expressions and defintions
 // pattern {action}
 
+//char      ::== a|b|c ... z
 
-*/
+
+
+
+
+
+
+
+
+
 
 //translation rules
 /*
@@ -22,8 +34,44 @@
 */
 
 //auxiliary functions
+//scans through each character of input text
+function lexer(){
+	
+	var letter = /[a-z]/;
+var digit = /[0-9]/;
+var space = /\s/;
+var ws = /[space]+/;
+var id = /[letter]/;
+var intexpr = /[digit]+/;
+	var inputText = document.getElementById("textInput").value;
+
+	//document.getElementBYId("output").value =  inputText;
+
+
+	
+	for (var i=0; i<inputText.length;i++){
+		alert(i);
+		alert('this is the length of the text: ' + inputText.length);
+	//start scanning the file at the first character
+	var c = inputText[i];
+	
+	if ( c.search(letter) != -1 ){
+		alert('LEXER: '+ c +'--> [CHAR]');
+	
+		
+	}
+	
+	
+	alert('this is the current c:'  + c);
+	
+	
+	}
+
+}
 
 /*
+
+
 
 
 
