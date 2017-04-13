5cb4902b256511206639ad95e21e3ecbfaaa6f8b
Colin <colinferris95@gmail.com>
2017-04-12 20:21:55 -0400

    basic symbol table generation

--- JS/symbolTable.js
+++ JS/symbolTable.js
@@ -1,4 +1,32 @@
-// code from crafting a compiler 8.1 
+/*
+	switch(node){
+		case 'block':
+			alert('symtab.openScope()');
+		case 'VarDecl':
+			alert('symtab.enterSymbol(node.name,node.type) var decl');
+		default:
+			if ( (node).search(letter) != -1){
+				alert('symtab.enterSymbol(node.name,node.type) default');
+			}
+	
+	<script type="text/javascript">
+    var typesHash = new Hashtable();
+
+    typesHash.put("A string", "string");
+    typesHash.put(1, "number");
+
+    var o = {};
+    typesHash.put(o, "object");
+
+    alert( typesHash.get(o) ); // "object"
+</script>
+	
+		
+	}
+	*/
+	
+// code from crafting a compiler 8.1
+
 /*
 procedure BuildSymbolTable()
 	call processNode(ASTroot)
@@ -48,11 +76,13 @@ public class Env{
 //ASTREE.root.name
 
 var letter = /[a-z]/;
+var table = new symbolTable();
 
 function start(){
 console.log("this is the AST from AST.js " + ASTREE.getNodes());
-buildSymbolTable();
+processNode();
 }
+/*
 function buildSymbolTable(){
 	for (i = 0; i < ASTREE.getNodes().length; i++){
 		processNode(ASTREE.getNodes()[i]);
@@ -62,39 +92,100 @@ function buildSymbolTable(){
 	
 	
 }
+*/
+
+function processNode(){
+	
+	for (i = 0; i < ASTREE.getNodes().length; i++){
+	//alert(ASTREE.getNodes()[i]);
+	
+	
+	if (ASTREE.getNodes()[i] == 'block'){
+		table.openScope();
+	}
+	else if (ASTREE.getNodes()[i] == 'VarDecl'){
+		//alert('symtab.enterSymbol(node.name,node.type) var decl');
+		i++;
+		//alert(ASTREE.getNodes()[i]);
+		//alert('node type');
+		var type = ASTREE.getNodes()[i];
+		i++;
+		//alert(ASTREE.getNodes()[i]);
+		//alert('node name');
+		var name = ASTREE.getNodes()[i];
+		
+		table.enterSymbol(type,name);
+	
+	}
+	else if ( (ASTREE.getNodes()[i]).search(letter) != -1 && ((ASTREE.getNodes()[i]).length) <= 2){
+		var name = ASTREE.getNodes()[i];
+		//alert('symtab.retrieveSymbol(node.name,node.type) default');
+		table.retrieveSymbol(name);
+	}
+	
+
+	}
+	table.outputTable();
+}
 
 
-function processNode(node){
-	alert(node);
-	/*
-	switch(node){
-		case 'block':
-			alert('symtab.openScope()');
-		case 'VarDecl':
-			alert('symtab.enterSymbol(node.name,node.type) var decl');
-		default:
-			if ( (node).search(letter) != -1){
-				alert('symtab.enterSymbol(node.name,node.type) default');
+
+
+
+function symbolTable(){
+	var symtable = [];
+	var scopePointer = -1;
+	var i = 0;
+	
+	this.openScope = function(){
+		scopePointer++
+			
+	}
+	this.closeScope = function(){
+		
+	}
+	
+	this.enterSymbol = function (type,name){
+	   //alert('hey look' + type);
+	   
+		symtable.push([type,name,scopePointer]);
+		i++
+		console.log(symtable);
+		
+	}
+	
+	this.retrieveSymbol = function(name){
+		
+		for (j = 0; j < symtable.length; j++){
+			//alert(symtable[j][1]);
+			if (symtable[j][1] == name){
+				//alert('symbol is declared, no error');
+				
 			}
-	
-
-	
+			else{
+				//alert('symbol has not been declared, ERROR');
+				
+			}
+			
+		}
 		
 	}
-	*/
 	
-	if (node == 'block'){
-		alert('symtab.openScope()');
-	}
-	else if (node == 'VarDecl'){
-		alert('symtab.enterSymbol(node.name,node.type) var decl');
+	this.outputTable = function(name){
+		document.getElementById("AStree").value +=' \n' + 'Name  Type  Scope ';
+		for (x = 0; x < symtable.length; x++){
+			document.getElementById("AStree").value += ' \n';
+			document.getElementById("AStree").value += ' ' + symtable[x][1];
+			document.getElementById("AStree").value += '        ' + symtable[x][0];
+			document.getElementById("AStree").value += '        ' + symtable[x][2];
+			
+			
+		}
+		
 		
 	}
-	else if ( (node).search(letter) != -1){
-		alert('symtab.enterSymbol(node.name,node.type) default');
-	}
 	
-
+	
 	
 }
 			
